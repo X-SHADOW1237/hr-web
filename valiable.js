d672b94c5763c44f67498833082bc7728c54bd6d
safari <speleji@gmail.com>
2024-08-22 03:34:11 -0700

    summary

---
+++ valiable.js
@@ -0,0 +1,16 @@
+var location="kirehe";
+adddress="Mahama V1/c5 A";
+const name="mugisha";
+location="kigali";
+const age="23";
+Boolean = true
+var Firstname = "safari";
+var Lastname = "peleji";
+var Age = "16" ;
+var Maritalstutus = "single";
+var person={Firstname:"safari", Lastname:"peleji"}
+var car={ type:"toyota", model:"corilla", color:"white"}
+
+console.log(Firstname,Lastname,Age,Maritalstutus)
+console.log("My name is"+Firstname+""+Lastname+ "I am)
+console.log(person.Firstname);
